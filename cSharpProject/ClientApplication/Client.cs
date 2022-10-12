748da1a4abc2c4e9601346266b3b6811e69e8fe8
Jasper Hopmans <Jasperhopmans@gmail.com>
2022-10-12 23:16:24 +0200

    Begin Server Client

---
+++ cSharpProject/ClientApplication/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApplication;
+
+class Client
+{
+    public Client()
+    {
+        
+        string hostName = Dns.GetHostName();
+        string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+        TcpClient client = new TcpClient(IPAddress.Parse(myIP).ToString(), 3370);
+        bool done = false;
+        Console.WriteLine("Type 'Bye' to end connection");
+        while (!done)
+        {
+            //Console.WriteLine("Enter a message to the server: ");
+            string message = Console.ReadLine();
+
+            WriteMessage(client, message);
+
+            string response = ReadTextMessage(client);
+            Console.WriteLine($"Response: {response}");
+            done = response.Equals("Bye");
+        }
+    }
+
+    private static string ReadTextMessage(TcpClient client)
+    {
+        var stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+        {
+            return stream.ReadLine();
+        }
+    }
+
+    private static void WriteMessage(TcpClient client, string message)
+    {
+        var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+        {
+            stream.WriteLine(message);
+            stream.Flush();
+        }
+    }
+}
---
+++ cSharpProject/SharedProject/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace SharedProject
+{
+  class Class1
+  {
+  }
+}
